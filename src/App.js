8bac52eba2ea6fd45e71c26596a8e147b64987c0
Harish Kumar <hkumar@audyglobal.in>
2022-07-08 16:29:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ModelViewer from './modalViewer';
+import { useParams } from 'react-router-dom';
 
 function App() {
+  const params = useParams()
+  console.log(params)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ModelViewer scale="40" modelPath={params?.url} />
   );
 }
 
